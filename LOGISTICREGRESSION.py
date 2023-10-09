f89d52e24c123e2ae091b626f5d9fab5bb29fd4c
nargismukimova <146948970+nargismukimova@users.noreply.github.com>
2023-10-09 11:43:08 +0500

    Add files via upload

--- LOGISTICREGRESSION.py
+++ LOGISTICREGRESSION.py
@@ -3,67 +3,33 @@ import pickle
 import pandas as pd 
 import streamlit as st
 
-from PIL import Image
+pickle_in = open("LOGISTICREGRESSION.pkl", "rb")
+regressor = pickle.load(pickle_in)
 
-
-pickle_in = open("LOGISTICREGRESSION.pkl","rb")
-regressor=pickle.load(pickle_in)
-
-def predict_survived(gender, Age, SibSp, Parch, Fare):
-    
-   
-    prediction=regressor.predict([[gender, Age, SibSp, Parch, Fare]])
-    print(prediction)
+def predict_survived(gender, age, sibsp, parch, fare):
+    prediction = regressor.predict([[gender, age, sibsp, parch, fare]])
     return prediction
 
-
-
 def main():
-    st.title("Прогноз выжившив на Титанике")
-    html_temp = """
-    <div style="background-color:green    ;padding:10px">
-    <h2 style="color:white;text-align:center;">Учебная модель поможет предсказать выживет ли человек на Титанике. МОдель обучена на основе данных titanic, алгоритмом GrdBosstingReg (точность 76%) </h2>
-    </div>
-    """
-    st.markdown(html_temp,unsafe_allow_html=True)
+    st.title("Прогноз выживания на Титанике")
+    st.markdown("Учебная модель поможет предсказать выживет ли человек на Титанике. Модель обучена на основе данных Titanic с использованием алгоритма GrdBosstingReg (точность 76%).")
     
-    m=st.radio("Пол:",
-        key="gender",
-        options=["Мужской", "Женский"],    )
-    if m=="Мужской":
-        market_code=0
-    elif m=="Женский":
-        market_code=1
-        
+    gender = st.radio("Пол:", options=["Мужской", "Женский"])
+    age = st.slider("Возраст:", min_value=0, max_value=100, step=1)
+    sibsp = st.slider("Количество братьев/сестер на борту:", min_value=0, max_value=10, step=1)
+    parch = st.slider("Количество родителей/детей на борту:", min_value=0, max_value=10, step=1)
+    fare = st.number_input("Стоимость билета:", min_value=0.0, format="%.2f")
     
-  #  rooms = st.text_input("Количество комнат","")
-   # floor = st.text_input("Этаж","")
-    #area = st.text_input("Площадь в кв.м.","")
-    
-   # r = st.radio("Ремонт:",
-    #             key="Remodel",
-     #            options=["Нет ремонта(коробка)", "Старый ремонт", "Новый ремонт"], )
-   # if r == "Нет ремонта(коробка)":
-    #    remodel_code = 0
-    #elif r == "Старый ремонт":
-     #   remodel_code = 1
-    #elif r == "Новый ремонт":
-     #   remodel_code = 2
-    
-    
-    
-    
-#    result=""
- #   if st.button("Predict"):
-  #      result=int(predict_house_price(market_code, rooms, floor, area, remodel_code))
-    
-   # st.success('Оценочная стоимость квартиры {}'.format(result)+" Сомони")
-     
-     
-    
+    if st.button("Предсказать"):
+        prediction = predict_survived(gender, age, sibsp, parch, fare)
+        st.success(f"Предсказание: {prediction}")
+
+if __name__ == '__main__':
+    main() 
     
     
 
-if __name__=='__main__':
-    main()
-    
+
+     
+     
+
